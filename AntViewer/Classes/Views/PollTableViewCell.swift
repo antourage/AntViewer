6d37eb8a80c5ee36fabdf4826cd4a719b81f7ef7
Maryn Luchko <ml@leobit.com>
2020-04-22 22:31:34 +0300

    Fixed PollTableViewCell appearance.

--- AntViewer/Classes/Views/PollTableViewCell.swift
+++ AntViewer/Classes/Views/PollTableViewCell.swift
@@ -12,11 +12,17 @@ class PollTableViewCell: UITableViewCell {
   @IBOutlet weak var titleLabel: UILabel!
   @IBOutlet weak var percentageLabel: UILabel!
 
-  lazy var progressLayer: CALayer = {
-    let layer = CALayer()
-    cardView.layer.insertSublayer(layer, at: 0)
-    return layer
+  lazy var progressView: UIView = {
+    let progressView = UIView()
+    cardView.insertSubview(progressView, at: 0)
+    progressView.translatesAutoresizingMaskIntoConstraints = false
+    progressView.leftAnchor.constraint(equalTo: cardView.leftAnchor).isActive = true
+    progressView.topAnchor.constraint(equalTo: cardView.topAnchor).isActive = true
+    progressView.bottomAnchor.constraint(equalTo: cardView.bottomAnchor).isActive = true
+    return progressView
   }()
+
+  private var progressViewWidthConstrant: NSLayoutConstraint?
 
   var isStatistic = false {
     didSet {
@@ -32,25 +38,25 @@ class PollTableViewCell: UITableViewCell {
       UIColor.pink :
       UIColor.cellGray
       percentageLabel.textColor = color
-      progressLayer.backgroundColor = color.withAlphaComponent(0.33).cgColor
+      progressView.backgroundColor = color.withAlphaComponent(0.33)
       cardView.layer.borderColor = color.cgColor
     }
   }
 
   var percentage: Int = 0 {
     didSet {
-      percentageLabel.text = String(format: "%d", percentage)
+      guard isStatistic else { return }
+      percentageLabel.text = String(format: "%d%%", percentage)
+      progressViewWidthConstrant?.isActive = false
+      progressViewWidthConstrant = nil
+      progressViewWidthConstrant = progressView.widthAnchor.constraint(equalTo: cardView.widthAnchor, multiplier: CGFloat(percentage)/100)
+      progressViewWidthConstrant?.isActive = true
+      cardView.layoutSubviews()
     }
   }
 
   override func awakeFromNib() {
     super.awakeFromNib()
     cardView.layer.borderColor = UIColor.pink.cgColor
-  }
-
-  override func setNeedsDisplay() {
-    super.setNeedsDisplay()
-    guard isStatistic else { return }
-    progressLayer.frame = CGRect(origin: .zero, size: CGSize(width: cardView.bounds.width*(CGFloat(percentage)/100), height: cardView.bounds.height))
   }
 }
